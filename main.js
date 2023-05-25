65d142fe1a34cb8aa002ee63256d464bf432736b
jonathanrichardss <jonathanrichard45@hotmail.com>
2023-05-25 00:08:26 -0300

    Commit prod.

--- main.js
+++ main.js
@@ -640,7 +640,7 @@ window.checkHrefChange = function checkHrefChange() {
   let isLogado = window.validaUserIsLogado();
 
   if (!isLogado && location.href !== 'cadastro.html') {
-    location.href = "https://mtools-adm.vercel.app/login.html";
+    location.href = "login.html";
     isDone = true;
   }
   if (isDone) {
